801f8c193ba79b6a1cdc847aef81f97d9d9b05eb
Erik Olson <erik@Olson.host>
2023-05-28 19:16:19 -0400

    wip

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if ($rows) {
 ##    ## ##     ##    ###    ########  ##     ## ##     ##
 -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Twilite</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
--- javascript.js
+++ javascript.js
@@ -18,12 +18,13 @@ function showCode(data) {
     console.log(data);
     $('#code').text(data.code);
     $('.save').removeClass('disabled');
+    $('.leftside').html('<h3>' + data.To + '</h3><ul><li>New Function</li><li>New Library</li><li><ol><li>Main</li></ol></li></ul>')
 }
 
 $('.save').on('click', function() {
-
+    $('.bottomside').html('Program Saved');
     // Save the contents of #code back into the database
 
-    
+
 
 });
